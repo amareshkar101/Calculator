c6ff4063a4961901109114bc45520bd6fbcda20d
Amaresh Kar <67188555+theamareshkar@users.noreply.github.com>
2020-07-08 12:49:25 +0530

    Updates

---
+++ precache-manifest.9cc22f8f5abd7e8dcb6fa98f7af39276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7862701ab70b6da0d97ef0cc682059ab",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "c49d4fa23f6d4c062202",
+    "url": "/Calculator/static/css/main.d07a3c6a.chunk.css"
+  },
+  {
+    "revision": "c8464c54e2c771519e42",
+    "url": "/Calculator/static/js/2.2b7cfe64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.2b7cfe64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49d4fa23f6d4c062202",
+    "url": "/Calculator/static/js/main.d30f7460.chunk.js"
+  },
+  {
+    "revision": "8db31b064112002f5bad",
+    "url": "/Calculator/static/js/runtime-main.1d8ddf6c.js"
+  }
+]);
